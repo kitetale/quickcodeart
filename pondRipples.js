d60869d9c1123781ce12a9a4033d20fcad944cda
kitetale <72019592+kitetale@users.noreply.github.com>
2020-10-18 02:39:04 -0400

    Create pondRipples.js

---
+++ pondRipples.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+var ripples = [];
+
+function draw() {
+  background("rgba(0,142,174,0.1)");
+  stroke("rgb(100,200,222)");
+  strokeWeight(2);
+  noFill();
+  
+  for(let i=0; i<ripples.length; i++){
+    ripples[i].drawRip();
+  }
+}
+
+function mousePressed(){
+  ripples[ripples.length] = new Ripple(mouseX,mouseY,1);
+}
+
+class Ripple{
+  constructor(x,y,r){
+    this.x = x;
+    this.y = y;
+    this.r =r;
+  }
+  
+  drawRip(){
+    this.r += 1;
+    ellipse(this.x,this.y,this.r);
+  }
+}
